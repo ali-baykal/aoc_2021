9cf188c5f0ca18d58ece807c1020801c3f78a82c
Ali Baykal <alib@dasburo.com>
2021-12-04 19:00:32 +0100

    (2.1): Solve part one of day two

---
+++ 02/part1.hs
@@ -0,0 +1,55 @@
+import Distribution.Simple.Program.HcPkg (list)
+data Position = Position{
+    depth:: Int ,
+    horizontal:: Int 
+}
+
+tuplify2 :: [a] -> (a,a)
+tuplify2 [x,y] = (x,y)
+tuplify2 _ = error "List with more aor less than two items"
+
+group :: Int -> [a] -> [[a]]
+group n l
+    | n < 0 = error "Negative or zero n"
+    | n <= length l = (take n l) : (group n $ drop n l)
+    | n > length l = []
+    | otherwise = error "Non groupable list"
+
+convertToTuples::[String] -> [(String, String )]
+convertToTuples = map tuplify2 . group 2
+
+convertParameter::String -> Int
+convertParameter parameter = read parameter::Int
+
+goDown :: Int -> Position -> Position
+goDown x Position{depth = d, horizontal = h} = Position {depth = d + x, horizontal= h}
+
+goUp :: Int -> Position -> Position
+goUp x Position{depth = d, horizontal = h} = Position {depth = d - x, horizontal= h}
+
+goForward :: Int -> Position -> Position
+goForward x Position{depth = d, horizontal = h} = Position {depth = d, horizontal= h + x}
+
+convertCommand::String -> (Int-> Position -> Position)
+convertCommand "forward" = goForward
+convertCommand "down" = goDown
+convertCommand "up" = goUp
+convertCommand _ = error "Unknown command"
+
+convertToFunctionAndParameter :: (String, String) -> (Int -> Position -> Position, Int)
+convertToFunctionAndParameter (c,p) = (convertCommand c, convertParameter p)
+
+convertToModifier :: (Int-> Position -> Position, Int) -> (Position -> Position)
+convertToModifier (fn, p) = fn p
+
+multiplyPosition :: Position -> Int
+multiplyPosition Position{depth = d, horizontal = h} = d * h;
+
+
+main:: IO()
+main = do 
+    x <- readFile "./input"
+    let tuples = convertToTuples $ words x  
+    let modifiers = map (convertToModifier . convertToFunctionAndParameter) tuples
+    let finalPosition = foldl (\acc fn -> fn acc) Position{horizontal = 0, depth =0} modifiers
+    print $ multiplyPosition finalPosition

ee91a0da1c2cb21425f40928e06cd950cfa43dee
Ali Baykal <alib@dasburo.com>
2021-12-06 19:05:48 +0100

    (6.1): Solve part 1 day 6 lazily and naively

---
+++ 06/part1.hs
@@ -0,0 +1,17 @@
+type Population = [Int];
+
+testData :: [Int]
+-- testData = [3,4,3,1,2]
+testData = [5,1,2,1,5,3,1,1,1,1,1,2,5,4,1,1,1,1,2,1,2,1,1,1,1,1,2,1,5,1,1,1,3,1,1,1,3,1,1,3,1,1,4,3,1,1,4,1,1,1,1,2,1,1,1,5,1,1,5,1,1,1,4,4,2,5,1,1,5,1,1,2,2,1,2,1,1,5,3,1,2,1,1,3,1,4,3,3,1,1,3,1,5,1,1,3,1,1,4,4,1,1,1,5,1,1,1,4,4,1,3,1,4,1,1,4,5,1,1,1,4,3,1,4,1,1,4,4,3,5,1,2,2,1,2,2,1,1,1,2,1,1,1,4,1,1,3,1,1,2,1,4,1,1,1,1,1,1,1,1,2,2,1,1,5,5,1,1,1,5,1,1,1,1,5,1,3,2,1,1,5,2,3,1,2,2,2,5,1,1,3,1,1,1,5,1,4,1,1,1,3,2,1,3,3,1,3,1,1,1,1,1,1,1,2,3,1,5,1,4,1,3,5,1,1,1,2,2,1,1,1,1,5,4,1,1,3,1,2,4,2,1,1,3,5,1,1,1,3,1,1,1,5,1,1,1,1,1,3,1,1,1,4,1,1,1,1,2,2,1,1,1,1,5,3,1,2,3,4,1,1,5,1,2,4,2,1,1,1,2,1,1,1,1,1,1,1,4,1,5]
+
+
+simulate:: Population -> Int -> Population
+simulate x 0 = x
+simulate [] _ = []
+simulate population days = 
+    simulate nextGen (days - 1)
+    where
+        decreased = map (\timer -> timer - 1) population
+        completedPopulation = filter (>= 0) decreased
+        timerUpCount = length population - length completedPopulation
+        nextGen = completedPopulation ++ replicate timerUpCount 6 ++ replicate timerUpCount 8

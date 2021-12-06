2b7eb658ee1e5f8734c4a0a3653241f91cc06a03
Ali Baykal <alib@dasburo.com>
2021-12-06 19:06:51 +0100

    (6.2): Solve part 2 day 6 without parsing

---
+++ 06/part2.hs
@@ -0,0 +1,30 @@
+type Population = [Int];
+
+testData :: [Int]
+-- testData = [3,4,3,1,2]
+testData = [5,1,2,1,5,3,1,1,1,1,1,2,5,4,1,1,1,1,2,1,2,1,1,1,1,1,2,1,5,1,1,1,3,1,1,1,3,1,1,3,1,1,4,3,1,1,4,1,1,1,1,2,1,1,1,5,1,1,5,1,1,1,4,4,2,5,1,1,5,1,1,2,2,1,2,1,1,5,3,1,2,1,1,3,1,4,3,3,1,1,3,1,5,1,1,3,1,1,4,4,1,1,1,5,1,1,1,4,4,1,3,1,4,1,1,4,5,1,1,1,4,3,1,4,1,1,4,4,3,5,1,2,2,1,2,2,1,1,1,2,1,1,1,4,1,1,3,1,1,2,1,4,1,1,1,1,1,1,1,1,2,2,1,1,5,5,1,1,1,5,1,1,1,1,5,1,3,2,1,1,5,2,3,1,2,2,2,5,1,1,3,1,1,1,5,1,4,1,1,1,3,2,1,3,3,1,3,1,1,1,1,1,1,1,2,3,1,5,1,4,1,3,5,1,1,1,2,2,1,1,1,1,5,4,1,1,3,1,2,4,2,1,1,3,5,1,1,1,3,1,1,1,5,1,1,1,1,1,3,1,1,1,4,1,1,1,1,2,2,1,1,1,1,5,3,1,2,3,4,1,1,5,1,2,4,2,1,1,1,2,1,1,1,1,1,1,1,4,1,5]
+-- testData = [6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8]
+
+transformInitialData' :: Population -> Int -> Population
+transformInitialData' population timer = 
+     h ++ (timerCount : (tail t))
+            where
+                splitPopulation = splitAt timer population
+                h = fst splitPopulation
+                t = snd splitPopulation
+                timerCount = (+1) $ head t
+
+transformInitialData :: [Int] -> Population
+transformInitialData  = foldl transformInitialData' (replicate 9 0)
+
+simulate:: Int ->Population -> Population
+simulate 0 population = population
+simulate days population = 
+    simulate (days - 1) nextGen
+    where
+        created = head population 
+        populationWithCreated = (tail population) ++ [created]
+        h = take 6 populationWithCreated
+        t = drop 6 populationWithCreated
+        withTimer6 = (head t) + created
+        nextGen = h ++ (withTimer6 : (tail t))
